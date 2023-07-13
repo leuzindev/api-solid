29e014f91757b9b3405d580c01b8d251222579b9
Leonardo_Soares <leonardoc.soares08@gmail.com>
2023-07-13 11:29:48 -0300

    feat: test for user repository

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,59 @@
-import { expect, test } from 'vitest'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register-usecase'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositorys/in-memory/in-memory-users-repository'
 
-test('Check', () => {
-  expect(2 + 2).toBe(5)
+describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John doe',
+      email: 'Johndoe@exemple.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John doe',
+      email: 'Johndoe@exemple.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'Johndoe@exemple.com'
+
+    await registerUseCase.execute({
+      name: 'John doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
 })
